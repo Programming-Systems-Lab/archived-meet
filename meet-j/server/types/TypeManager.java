7ebd51cd617fb746e24e6926b7811f639224b65c
phil <phil>
2002-08-07 22:30:09 +0000

    no message

---
+++ meet-j/server/types/TypeManager.java
@@ -0,0 +1,37 @@
+/*
+ * TypeManager.java
+ *
+ * Created on August 7, 2002, 5:11 PM
+ */
+
+package psl.meet.server.types;
+
+/**
+ * @author phil
+ */
+public class TypeManager extends Object {
+    
+    /** Creates new TypeManager */
+    public TypeManager() {
+    }
+    
+    /*
+    private String examplePrivateMethod() {
+      return "examplePrivateMethod value";
+    }
+     */
+    
+    public static class TEST extends Object {
+        
+        /** Unit-test {@link TypeManager}.
+         * @param args the command line arguments
+         */
+        public static void main(String args[]) {
+            /*
+            System.err.println("Test results: " + new TypeManager().examplePrivateMethod());
+             */
+        }
+        
+    }
+    
+}
