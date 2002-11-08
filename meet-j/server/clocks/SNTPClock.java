56ad526064f74ac483a892fe825a816e20a7011e
tmk29 <tmk29>
2002-11-08 01:33:33 +0000

    initial import

---
+++ meet-j/server/clocks/SNTPClock.java
@@ -0,0 +1,19 @@
+/*
+ * SNTPClock.java
+ *
+ * Created on November 7, 2002, 8:34 PM
+ */
+
+package psl.meet.server.clocks;
+
+/**
+ *
+ * @author  mkuba
+ */
+public class SNTPClock {
+    
+    /** Creates a new instance of SNTPClock */
+    public SNTPClock() {
+    }
+    
+}
